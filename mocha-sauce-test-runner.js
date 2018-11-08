4d4776d8ee2b799d821df7bac63011bf42b029b9
Rob Hicks <rob@hixfamily.org>
2018-11-08 03:38:07 -0700

    change port to 443

--- mocha-sauce-test-runner.js
+++ mocha-sauce-test-runner.js
@@ -26,7 +26,7 @@ const sauce = new MochaSauce({
 	username: process.env.SAUCE_USERNAME, // Sauce username
 	accessKey: process.env.SAUCE_ACCESS_KEY, // Sauce access key
 	host, // using Sauce Connect
-	port: 4445,
+	port: 443,
 	url // point to the site running your mocha tests
 });
 
